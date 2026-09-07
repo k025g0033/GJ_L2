e91fc705a4db368ee44c50f08f170952c6a01b0f
hayashi teruto <k025g0057@g.neec.ac.jp>
2026-09-07 13:57:14 +0900

    終わり

--- DirectXGame/StageSelectScene.h
+++ DirectXGame/StageSelectScene.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "IScene.h"
+#include "KamataEngine.h"
+#include <array>
 
 // ステージセレクトシーン
 class StageSelectScene : public IScene {
 public:
+	~StageSelectScene() override;
+
 	void Initialize() override;
 	void Update() override;
 	void Draw() override;
@@ -14,11 +18,19 @@ public:
 	}
 
 private:
+	void UpdateStageSpriteLayout();
+
 	bool isFinished_ = false;
 
 	// 1~3のステージを選択
 	int selectedStageNumber_ = 1;
+	int previousStageNumber_ = 1;
+	bool isAnimating_ = false;
+	float animationTime_ = 0.0f;
+	std::array<uint32_t, 4> stageTextureHandles_{};
+	std::array<KamataEngine::Sprite*, 4> stageSprites_{};
 
 	static inline const int kMinStageNumber = 1;
 	static inline const int kMaxStageNumber = 3;
+	static inline const float kAnimationDuration = 0.35f;
 };
