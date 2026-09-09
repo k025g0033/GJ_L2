7addf51df330b00ad21703e16e68314d4397bc0e
hayashi teruto <k025g0057@g.neec.ac.jp>
2026-09-09 16:09:55 +0900

    UIの追加

--- DirectXGame/ResultScene.cpp
+++ DirectXGame/ResultScene.cpp
@@ -59,10 +59,8 @@ void ResultScene::Update() {
 }
 
 void ResultScene::Draw() {
-	DebugText::GetInstance()->Print("RESULT SCENE", 480.0f, 200.0f, 2.0f);
 	Sprite::PreDraw();
 	titleSprite_->Draw();
 	stageSelectSprite_->Draw();
-	DebugText::GetInstance()->DrawAll();
 	Sprite::PostDraw();
 }
